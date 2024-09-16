5fe21e8ab42aac07e2d694e650b885ab894e9d15
Trismegiste <flo@trismegiste.net>
2024-09-16 10:34:33 +0200

    sse

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"main/darwin"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/django/v3"
+	"github.com/valyala/fasthttp"
 )
 
 func main() {
@@ -20,9 +23,42 @@ func main() {
 	app.Static("/es", "./public/es")
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
-		})
+		return c.Render("index", fiber.Map{})
+	})
+
+	app.Post("/run", func(c *fiber.Ctx) error {
+		return c.Render("run", fiber.Map{})
+	})
+
+	app.Get("/sse", func(c *fiber.Ctx) error {
+		c.Set("Content-Type", "text/event-stream")
+		c.Set("Cache-Control", "no-cache")
+		c.Set("Connection", "keep-alive")
+		c.Set("Transfer-Encoding", "chunked")
+
+		c.Status(fiber.StatusOK).Context().SetBodyStreamWriter(fasthttp.StreamWriter(func(w *bufio.Writer) {
+			fmt.Println("WRITER")
+			var i int
+			for {
+				i++
+				msg := fmt.Sprintf("%d - the time is %v", i, time.Now())
+				fmt.Fprintf(w, "data: Message: %s\n\n", msg)
+				fmt.Println(msg)
+
+				err := w.Flush()
+				if err != nil {
+					// Refreshing page in web browser will establish a new
+					// SSE connection, but only (the last) one is alive, so
+					// dead connections must be closed here.
+					fmt.Printf("Error while flushing: %v. Closing http connection.\n", err)
+
+					break
+				}
+				time.Sleep(2 * time.Second)
+			}
+		}))
+
+		return nil
 	})
 
 	log.Fatal(app.Listen(":3000"))
