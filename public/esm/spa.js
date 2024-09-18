6be59c9e56a9800d755414cba73e81587ea9eb9b
Trismegiste <flo@trismegiste.net>
2024-09-18 10:21:17 +0200

    prevent autojumping of graph

--- public/esm/spa.js
+++ public/esm/spa.js
@@ -4,7 +4,9 @@ export default (host) => ({
     epoch: 10,
     socket: null,
     statByCost: {},
-    maxes: {},
+    maxes: {
+        cost: 30
+    },
 
     run() {
         if (typeof(EventSource) === "undefined") {
@@ -28,8 +30,9 @@ export default (host) => ({
             } else {
                 const state = JSON.parse(event.data)
                 this.statByCost = state.InfoPerCost
+                const lastMax = this.maxes.cost // this line to prevent the graph continuously jumping, it's only growing
                 this.maxes = {
-                    cost: state.MaxCost,
+                    cost: state.MaxCost > lastMax ? state.MaxCost : lastMax,
                     count: state.MaxCount,
                     victory: state.MaxAvgVictory
                 }
