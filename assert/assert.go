b1129d6d2f36c2aa62038b556a30722eef31d096
Trismegiste <flo@trismegiste.net>
2024-09-19 17:22:09 +0200

    DRY

--- assert/assert.go
+++ assert/assert.go
@@ -7,6 +7,12 @@ import (
 
 func AssertFloat(t *testing.T, expected float64, testedValue float64, delta float64, message string) {
 	if math.Abs(expected-testedValue) > delta {
+		t.Fatal("In", message, ": Expecting", expected, "+/-", delta, "got", testedValue)
+	}
+}
+
+func AssertInt(t *testing.T, expected int, testedValue int, message string) {
+	if expected != testedValue {
 		t.Fatal("In", message, ": Expecting", expected, "got", testedValue)
 	}
 }
--- darwin/Fighter_test.go
+++ darwin/Fighter_test.go
@@ -1,7 +1,7 @@
 package darwin
 
 import (
-	"math"
+	"main/assert"
 	"testing"
 )
 
@@ -10,21 +10,10 @@ const enoughIteration = 1000000
 func TestFactory(t *testing.T) {
 	var f *Fighter = BuildFighter(8, 1, 6, 4, 4, 0)
 
-	if f.getFighting() != 8 {
-		t.Fatal("Fighting skill is not initialized to 8")
-	}
-
-	if f.getParry() != 7 {
-		t.Fatal("Parry should be equal to 8/2+2 + 1")
-	}
-
-	if f.getToughness() != 5 {
-		t.Fatal("Toughness should be equal to 6/2+2")
-	}
-
-	if f.genome[STRENGTH].get() != 4 {
-		t.Fatal("Strength should be equal to 4")
-	}
+	assert.AssertInt(t, 8, f.getFighting(), "Fighting skill initialisation")
+	assert.AssertInt(t, 7, f.getParry(), "Parry")
+	assert.AssertInt(t, 5, f.getToughness(), "Toughness")
+	assert.AssertInt(t, 4, f.genome[STRENGTH].get(), "Strength")
 }
 
 func TestAttackRoll(t *testing.T) {
@@ -35,9 +24,7 @@ func TestAttackRoll(t *testing.T) {
 	}
 
 	var avg float64 = float64(sum) / float64(enoughIteration)
-	if math.Abs(avg-8.0) > 0.1 {
-		t.Fatal("Average of joker attack at d12 is not around 8.0", "(", avg, ")")
-	}
+	assert.AssertFloat(t, 8.1, avg, 0.1, "Average of joker attack at d12")
 }
 
 func TestAttackRollWithBenny(t *testing.T) {
@@ -49,9 +36,7 @@ func TestAttackRollWithBenny(t *testing.T) {
 	}
 
 	var avg float64 = float64(sum) / float64(enoughIteration)
-	if math.Abs(avg-8.8) > 0.1 {
-		t.Fatal("Average of joker attack at d12 with a benny re-roll is not around 8.8", "(", avg, ")")
-	}
+	assert.AssertFloat(t, 8.8, avg, 0.1, "Average of joker attack at d12 with a benny re-roll")
 }
 
 func TestDamageRollWithCappedStrength(t *testing.T) {
@@ -63,10 +48,8 @@ func TestDamageRollWithCappedStrength(t *testing.T) {
 
 	var avg float64 = float64(sum) / float64(enoughIteration)
 	// Although the damage of the weapon is d8, the roll is capped by the strength (which is d4 here)
-	// therefore, we should get the average for 2d4R
-	if math.Abs(avg-6.7) > 0.1 {
-		t.Fatal("Average of damage roll is not consistent with 2d4R around 2*3.3", "(", avg, ")")
-	}
+	// therefore, we should get the average for 2d4R, therefore 2×3.35
+	assert.AssertFloat(t, 6.7, avg, 0.1, "Average of damage roll is not consistent with 2d4R")
 }
 
 func TestDamageRollWithStrength_D8(t *testing.T) {
@@ -77,10 +60,8 @@ func TestDamageRollWithStrength_D8(t *testing.T) {
 	}
 
 	var avg float64 = float64(sum) / float64(enoughIteration)
-	// therefore, we should get the average for 2d8R
-	if math.Abs(avg-10.3) > 0.1 {
-		t.Fatal("Average of damage roll is not consistent with 2d8R around 2*5.13", "(", avg, ")")
-	}
+	// therefore, we should get the average for 2d8R = 2×5.13
+	assert.AssertFloat(t, 10.3, avg, 0.1, "Average of damage roll is not consistent with 2d8R")
 }
 
 func TestDamageRollWithStrength_D12(t *testing.T) {
@@ -92,9 +73,7 @@ func TestDamageRollWithStrength_D12(t *testing.T) {
 
 	var avg float64 = float64(sum) / float64(enoughIteration)
 	// we should get the average for d8R+d12R
-	if math.Abs(avg-12.2) > 0.1 {
-		t.Fatal("Average of damage roll is not consistent with d8R+d12R around 5.1+7.1", "(", avg, ")")
-	}
+	assert.AssertFloat(t, 12.2, avg, 0.1, "Average of damage roll is not consistent with d8R+d12R")
 }
 
 func TestDamageRollWithStrength_D4_And_Benny(t *testing.T) {
@@ -106,33 +85,23 @@ func TestDamageRollWithStrength_D4_And_Benny(t *testing.T) {
 	}
 
 	var avg float64 = float64(sum) / float64(enoughIteration)
-	// we should get the average for 2d4R
-	if math.Abs(avg-8.5) > 0.1 {
-		// note that without the benny we should be around 6.7, see TestDamageRollWithCappedStrength
-		t.Fatal("Average of damage roll is not consistent with 2d4R and a benny around 8.5", "(", avg, ")")
-	}
+	assert.AssertFloat(t, 8.5, avg, 0.1, "Average of damage roll is not consistent with 2d4R and a benny")
 }
 
 func Test_ZeroFighterCost(t *testing.T) {
 	var f *Fighter = BuildFighter(4, 0, 4, 4, 4, 0)
 
-	if f.getCost() != 0 {
-		t.Fatal("Cost should be equal to 0")
-	}
+	assert.AssertInt(t, 0, f.getCost(), "Cost should be equal to 0")
 }
 
 func Test_MiddleFighterCost(t *testing.T) {
 	var f *Fighter = BuildFighter(12, 1, 6, 6, 6, 0)
 
-	if f.getCost() != (1+6)+2+2+2+2 {
-		t.Fatal("Cost should be equal to 15", f.getCost())
-	}
+	assert.AssertInt(t, (1+6)+2+2+2+2, f.getCost(), "Cost should be equal to 15")
 }
 
 func Test_HighFighterCost(t *testing.T) {
 	var f *Fighter = BuildFighter(12, 1, 10, 8, 10, 0)
 
-	if f.getCost() != (3+2)+2+6+4+6 {
-		t.Fatal("Cost should be equal to 23")
-	}
+	assert.AssertInt(t, (3+2)+2+6+4+6, f.getCost(), "Cost should be equal to 23")
 }
