65a807acb9c8540edba0a56d335d674d93aa5cbb
Trismegiste <flo@trismegiste.net>
2024-09-26 17:17:10 +0200

    PCA

--- pca.py
+++ pca.py
@@ -3,22 +3,18 @@ from sklearn import decomposition
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d  # if not present "projection=3d" (see below) is crashing
 
-pop = pd.read_json("sawo-export.json", lines=True)
-pop = pop.drop("Victory", axis=1)
-pop = pop.drop("Cost", axis=1)
+population = pd.read_json("sawo-export.json", lines=True)
+population = population.drop("Cost", axis=1)
 
-print(pop)
-X = pop
-
-pca = decomposition.PCA(n_components=3)
-pca.fit(X)
-X = pca.transform(X)
+X = population
 
 print(X)
+pca = decomposition.PCA()
+pca.fit(X)
 
-fig = plt.figure(1, figsize=(4, 3))
-plt.clf()
-ax = fig.add_subplot( projection="3d")
+print("Variance")
+print(pca.explained_variance_ratio_)
+print("Components")
+print(pca.components_)
 
-ax.scatter(X[:, 0], X[:, 1], X[:, 2])
-plt.show()
+# https://towardsdatascience.com/pca-clearly-explained-how-when-why-to-use-it-and-feature-importance-a-guide-in-python-7c274582c37e
