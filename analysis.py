5c8b8aabb6abb8c1e57c8540a566b524d3cd414b
Trismegiste <flo@trismegiste.net>
2024-09-30 15:11:58 +0200

    plot3d per trait

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn import linear_model
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d  # noqa: F401
 
 population = pd.read_json("sawo-export.json", lines=True)
 rowCount = population.axes[0].stop
@@ -17,3 +19,20 @@ pct = regr.coef_ * 100
 
 for idx, name in enumerate(X.columns):
     print(name, "\t% .1f%%" % pct[idx])
+
+
+#print("Plot max victory")
+fig = plt.figure(1, figsize=(4, 3))
+plt.clf()
+
+ax = fig.add_subplot(projection="3d")
+for k, trait in enumerate(['STR','Fight','Block','TradW','CmbRef','NervSt']):
+    subset = population.groupby([trait])
+    #print(subset)
+    stat = subset['Victory'].describe()
+    #print(stat)
+    for row in stat.iterrows():
+        ax.scatter(k, row[0], row[1]['max'])
+        #print(trait, row[0], row[1]['max'])
+
+plt.show()
